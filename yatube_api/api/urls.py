f095f7105575a659cffa2de63f444ccce32310c2
Sergey Sokolkin <hawkxdev@gmail.com>
2025-08-21 19:53:02 +0300

    refactor: update API routing to use a versioned router

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -5,16 +5,16 @@ from rest_framework.routers import DefaultRouter
 
 from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
 
-router = DefaultRouter()
-router.register('posts', PostViewSet)
-router.register('groups', GroupViewSet)
-router.register('follow', FollowViewSet, basename='follow')
-router.register(
+router_v1 = DefaultRouter()
+router_v1.register('posts', PostViewSet)
+router_v1.register('groups', GroupViewSet)
+router_v1.register('follow', FollowViewSet, basename='follow')
+router_v1.register(
     r'posts/(?P<post_id>\d+)/comments',
     CommentViewSet,
     basename='comments'
 )
 
 urlpatterns = [
-    path('', include(router.urls)),
+    path('', include(router_v1.urls)),
 ]
