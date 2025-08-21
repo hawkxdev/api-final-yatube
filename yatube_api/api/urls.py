b8a5595665d2bbe131fc32c2d25020ad43e8e986
Sergey Sokolkin <hawkxdev@gmail.com>
2025-08-21 18:03:18 +0300

    feat: implement API routing and ViewSets for posts, comments, groups, and follows

--- yatube_api/api/urls.py
+++ yatube_api/api/urls.py
@@ -1,4 +1,20 @@
-from django.urls import path
+"""URL роутинг для API."""
+
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from .views import CommentViewSet, FollowViewSet, GroupViewSet, PostViewSet
+
+router = DefaultRouter()
+router.register('posts', PostViewSet)
+router.register('groups', GroupViewSet)
+router.register('follow', FollowViewSet, basename='follow')
+router.register(
+    r'posts/(?P<post_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 urlpatterns = [
+    path('', include(router.urls)),
 ]
--- yatube_api/api/views.py
+++ yatube_api/api/views.py
@@ -1,1 +1,68 @@
-# TODO:  Напишите свой вариант
+"""ViewSets для API."""
+
+from django.shortcuts import get_object_or_404
+from rest_framework import filters, mixins, permissions, viewsets
+
+from posts.models import Group, Post
+from .permissions import AuthorOnlyEditPermission
+from .serializers import (
+    CommentSerializer, FollowSerializer, GroupSerializer, PostSerializer
+)
+
+
+class PostViewSet(viewsets.ModelViewSet):
+    """ViewSet для постов."""
+
+    queryset = Post.objects.all()
+    serializer_class = PostSerializer
+    permission_classes = (AuthorOnlyEditPermission,)
+
+    def perform_create(self, serializer):
+        """Автоматическая привязка автора."""
+        serializer.save(author=self.request.user)
+
+
+class CommentViewSet(viewsets.ModelViewSet):
+    """ViewSet для комментариев."""
+
+    serializer_class = CommentSerializer
+    permission_classes = (AuthorOnlyEditPermission,)
+
+    def get_queryset(self):
+        """Фильтрация по посту."""
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        return post.comments.all()
+
+    def perform_create(self, serializer):
+        """Автоматическая привязка автора и поста."""
+        post = get_object_or_404(Post, pk=self.kwargs.get('post_id'))
+        serializer.save(author=self.request.user, post=post)
+
+
+class GroupViewSet(viewsets.ReadOnlyModelViewSet):
+    """ViewSet для групп."""
+
+    queryset = Group.objects.all()
+    serializer_class = GroupSerializer
+    permission_classes = (permissions.AllowAny,)
+
+
+class FollowViewSet(
+    mixins.ListModelMixin,
+    mixins.CreateModelMixin,
+    viewsets.GenericViewSet
+):
+    """ViewSet для подписок."""
+
+    serializer_class = FollowSerializer
+    permission_classes = (permissions.IsAuthenticated,)
+    filter_backends = (filters.SearchFilter,)
+    search_fields = ('following__username',)
+
+    def get_queryset(self):
+        """Только подписки текущего пользователя."""
+        return self.request.user.follower.all()
+
+    def perform_create(self, serializer):
+        """Автоматическая привязка пользователя."""
+        serializer.save(user=self.request.user)
